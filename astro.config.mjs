4bf27ea984e40b2da5d1aab0989d032033fae675
Oleksii Levchenko <realcarter50@gmail.com>
2026-03-14 14:41:22 +0200

    Update site URL in Astro configuration from 'levchenko.dev' to 'levchenko.cc'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://levchenko.dev',
+  site: 'https://levchenko.cc',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
